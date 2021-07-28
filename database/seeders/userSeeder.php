ad01da20c7717ee0b27883bb932d839808f9e8ef
Muhammad Aku <muhammadali2388261@gmail.com>
2021-07-29 03:40:46 +0500

    Initial commit

---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //add new user
+        DB::table('users')->insert([
+            'name'=>"admin",
+            'email'=>"admin@gmail.com",
+            'password'=>Hash::make('123456789')
+        ]);
+
+
+    }
+}
